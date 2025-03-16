c514423276622961fa01b8a881573fa3d3f6052f
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2025-03-16 11:36:09 +0200

    Обновляет версию gulp-imagemin до 7.1.0 в package.json и package-lock.json. Добавляет условную оптимизацию изображений в задачу img в gulp, используя gulp-if для проверки окружения на продакшн.

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -2,20 +2,23 @@
  * Задача для обработки изображений
  */
 import gulp from 'gulp';
-// import imagemin from 'gulp-imagemin';
+import imagemin from 'gulp-imagemin';
 import webp from 'gulp-webp';
 import svgstore from 'gulp-svgstore';
 import rename from 'gulp-rename';
+import gulpif from 'gulp-if';
 import { paths, webpOptions, svgSpriteOptions } from '../../config.js';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 // Оптимизация изображений
 export const img = () => {
-  return (
-    gulp
-      .src(paths.src.img)
-      // .pipe(imagemin())
-      .pipe(gulp.dest(paths.build.img))
-  );
+  return gulp
+    .src(paths.src.img)
+    .pipe(gulpif(isProd, imagemin({
+      verbose: true // Выводим информацию об оптимизации
+    })))
+    .pipe(gulp.dest(paths.build.img));
 };
 
 // Конвертация изображений в WebP
